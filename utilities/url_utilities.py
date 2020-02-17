c5da27803dd175b67ce631ed4cc4cbbee5c3e3c3
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-02-17 07:19:13 +0000

    Added code to scrape_page function in url.utilities.py

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,4 +1,7 @@
+import re
+import string
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 
 def load_urls_from_file(file_path: str):
@@ -18,5 +21,44 @@ def load_page(url: str):
 
 
 def scrape_page(page_contents: str):
-    # TODO analyse the text
-    pass
+    chicken_noodle = BeautifulSoup(page_contents, "html5lib")
+
+    for script in chicken_noodle(["script", "style"]):
+        script.extract()
+
+    text = chicken_noodle.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+
+    text = ' '.join(chunk for chunk in chunks if chunk)
+    plain_text = ''.join(filter(lambda x: x in string.printable, text))
+
+    clean_words = []
+
+    words = plain_text.split(" ")
+    for word in words:
+        clean = True
+
+        # no punctuation
+        for punc in string.punctuation:
+            if punc in word:
+                clean = False
+
+            # no numbers
+            if any(char.isdigit() for char in word):
+                clean = False
+
+            # at least two characters but no more than 10
+            if len(word) < 2 or len(word) > 10:
+                clean = False
+
+            if not re.match(r'^\w+$', word):
+                clean = False
+
+            if clean:
+                try:
+                    clean_words.append(word.lower())
+                except UnicodeEncodeError:
+                    print(".")
+
+    return clean_words
